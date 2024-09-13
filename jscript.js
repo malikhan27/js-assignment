f8f2232cd8d78ae77f15a255b673e45af74eb86f
Muhammad Ali <alikhancss27@gmail.com>
2024-09-13 18:22:14 +0500

    js assignment

---
+++ jscript.js
@@ -0,0 +1,335 @@
+//chapter 1
+//qustion1
+alert("HELLO WELCOME TO OUR WEBSITE")
+
+alert("ERROR! Please enter a valid password")
+
+alert("Welcome to JS land....\nHappy coding")
+
+alert("Welcome to JS land")
+alert("Happy Coding! \n prevent this page from creating additional dialogs")
+
+document.write("<h1>CHAPTER NO.2</h1> " + " <h2> Q# 1 , 2 </h2> <br>")
+
+var username 
+var myName= "Mohammad Ali"
+
+document.write(myName)
+
+//Q#3
+
+var message 
+message="Hello World"
+
+alert("QUESTION NO.3 \n" + message)
+
+//Q#4
+
+var firstname= "Jhon"
+var age = 15
+var qualification= "Web Development"
+
+alert("Question no. 4 \n "+firstname)
+alert(age)
+alert(qualification)
+
+//Q#5
+
+var new_item= "Pizza\nPizz\nPiz\nPi\nP"
+
+alert("Question 5\n"+new_item)
+
+//Q#6
+
+var email="xyz@gmail.com"
+
+alert("Question#6 \n My Email is "+ email)
+
+//Q#7
+
+var book= "A Smarter Way To Learn Javascript"
+
+alert(" Question no. 7 \n I am learning Javascript from this book " + book)
+
+//Q#8
+document.write("<h2> Q # 8 </h2> <br>")
+
+document.write("Yah! I can write HTML through JS")
+
+//Q#9
+
+var new_symbol= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert(new_symbol)
+
+
+
+//chp3 
+
+//Q1
+var age = 15
+
+alert("chapter 3 \n Question 1 \n I am "+age+" years old ")
+
+//Q2
+var visits=110
+
+alert("Question 2 \n You have visited this page " + visits + " times")
+
+document.write("<h1>CHAPTER NO.3</h1> " + " <h2> Q# 3 </h2> <br>")
+
+var birthYear = 1990
+
+document.write("My birth year is " + birthYear + " <br> Datatype of my declared variable is number")
+
+document.write("<h2> Q# 4 </h2> <br>")
+
+var customer="jhon"
+var productTitle="T-shirts"
+var order= 5
+
+document.write(" MR. "+customer +" has odered " + order + productTitle + " from XYZstore.com " )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//chapter5 
+
+document.write("<h1>CHAPTER NO.5</h1> " + " <h2> Q# 1 , 2 , 3 </h2> <br>")
+
+//question1
+
+var a = 34
+var b = 12
+
+var c = a + b
+
+document.write( " the sum of " + a + " and " + b + " = " + c + "<br></br>")
+
+
+//question 2
+
+var c = a - b
+
+document.write(" the result of " + a + " - " + b + " = " + c  + "<br></br>")
+
+
+var c = a / b
+
+
+document.write(" the sum of division of " + a + " and " + b + " = " + c + "<br></br>")
+
+
+
+
+var c = a * b
+
+document.write(" the sum of multiplication of " + a + " and " + b + " = " + c + "<br></br>")
+
+
+
+var c = a % b
+
+document.write(" the modulos of " + a + " and " + b + " = " + c + "<br></br>" )
+
+//Qusetion no 3
+
+//a
+
+var num1 ;
+
+//b
+document.write(num1 +  " “Value after variable declaration is: ??” " + "<br> <br>")
+
+//c
+num1 = 5
+
+//d
+document.write(num1 + " “Initial value: 5”. " + "<br> <br>")
+
+//e
+++num1
+
+//f
+document.write(num1 + "  “Value after increment is: 6”.  " + "<br> <br>")
+
+//g,h
+
+//i
+num1= num1 + 7
+
+document.write(num1 + " “Value after addition is: 13”. " + "<br> <br>" )
+
+//h
+num1--
+
+//j
+document.write ( num1+ " “Value after decrement is: 12”. " + "<br> <br>")
+
+//k
+document.write( num1%3 + " “The remainder is : 0” ")
+
+//Q#4
+document.write("<h2> Q# 4 </h2> <br>")
+
+var ticketPrice = 600
+
+var totalcost = ticketPrice*5
+
+document.write("the total cost to 5 ticket price is " + totalcost)
+
+//Q#5
+document.write("<h2> Q# 5 </h2> <br>")
+
+var tablenum = 6
+
+document.write("6  x  1 = " + tablenum*1 + "<br>")
+document.write("6  x  2 = " + tablenum*2 + "<br>")
+document.write("6  x  3 = " + tablenum*3 + "<br>")
+document.write("6  x  4 = " + tablenum*4 + "<br>")
+document.write("6  x  5 = " + tablenum*5 + "<br>")
+document.write("6  x  6 = " + tablenum*6 + "<br>")
+document.write("6  x  7 = " + tablenum*7 + "<br>")
+document.write("6  x  8 = " + tablenum*8 + "<br>")
+document.write("6  x  9 = " + tablenum*9 + "<br>")
+document.write("6  x  10 = " + tablenum*10 + "<br>")
+
+
+//Q#6
+document.write("<h2> Q# 6 </h2> <br>")
+
+var celsius = 32
+
+celsiustoFahrenheit = (celsius*9/5) + 32
+
+document.write( celsius + " C is " + celsiustoFahrenheit + " F <br> "  )
+
+var fahrenheit= 90
+
+var fahrenheittoCelsius= (fahrenheit-32)*5/9
+
+document.write(fahrenheit+" F " + " is " + fahrenheittoCelsius + " C ")
+
+
+//Q#7
+document.write("<h2> Q# 7 </h2> <br>")
+
+var item_num1p = 500
+
+var total_item1 = 4
+
+var item_num2p=200
+
+var total_item2= 2
+
+var shippingCharges= 150
+
+var totalamount= (500*4)+(200*2)+150
+
+document.write( "PRICE OF ITEM NO.1 is = " + item_num1p + "<br> <br> TOTAL ORDER OF ITEM NO.1 is = " + total_item1 + "<br> <br>" )
+document.write( "PRICE OF ITEM NO.2 is = " + item_num2p + "<br> <br> TOTAL ORDER OF ITEM NO.2 is = " + total_item2+ "<br> <br> SHIPPING CAHRGES are = "+ shippingCharges +"<br><br>" )
+document.write("Total Cost Of This Order is = " + totalamount)
+
+
+//Q#8
+document.write("<h2> Q# 8 </h2> <br>")
+
+var markObtained = 450
+var totalMarks = 600
+var total_percentage = markObtained/totalMarks*100
+
+document.write("TOTAL MARKS  = " + 600 + "<br> MARKS THAT YOU HAVE OBTAINED = " + markObtained + " <br> ")
+document.write("YOUR PERCENTAGE IS " + total_percentage + "<br><br>")
+
+
+//Q#9
+document.write("<h2> Q# 9 </h2> <br>")
+
+var dollar = 104.88
+var riyal = 28
+var totalDollar= 10
+var totalRiyal= 25
+
+var totalCurrencyPKR= (dollar*totalDollar)+(riyal*totalRiyal)
+
+document.write("Total Currency In PKR is"+ totalCurrencyPKR + "<br> <br>")
+
+//Q#10
+document.write("<h2> Q# 10 </h2> <br>")
+
+
+var digit1= 23
+
+var expressions= digit1+5*10/2
+
+document.write("Following Expression Result is "+ expressions)
+
+//Q#11
+document.write("<h2> Q# 11 </h2> <br>")
+
+var currentYear= 2024
+var birthYear= 1999
+
+var age = currentYear-birthYear
+
+document.write("CURRENT YEAR = "+ currentYear +"<br> BIRTH YEAR = " + birthYear +"<br> Age = " + age +"<br> <br>")
+
+//Q#12
+document.write("<h2> Q# 12 </h2> <br>")
+
+var radius= 20
+var circumferenceofCircle= 2*3.142*20
+var areaofCircle= 3.142*20*20
+
+document.write("RADIUS OF CIRCLE = " + radius + " <br> CIRCUMFERENCE OF CIRCLE = " + circumferenceofCircle +"<br> AREA OF CIRCLE =" + areaofCircle +"<br><br>")
+
+//Q#13
+document.write("<h2> Q# 13 </h2> <br>")
+
+var favouriteFood= "BANANA"
+var currentAge= 25
+var maxAge= 75
+var amountPeryear= 5
+var finalamount = (maxAge-currentAge)*amountPeryear
+
+document.write("MY FAVOURITE FOOD : " + favouriteFood + "<br>")
+document.write("CURRENT AGE = " + currentAge + "<br>")
+document.write("ESTIMATED MAXIMUM AGE = " + maxAge + "<br>")
+document.write("AMOUNT CONSUMING PER YEAR : " + amountPeryear + "<br>")
+document.write("You will need " + finalamount + " to last you until the ripe old age of " + maxAge)
+
+//chapter#5complete
+
+
+
+
+
